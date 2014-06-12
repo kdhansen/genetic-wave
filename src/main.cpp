b0f20066d6557421bbf7e339fb7bb53b73a1d7b8
Karl Damkjær Hansen <kdh@es.aau.dk>
2014-06-12 16:28:35 +0200

    Initiate file structure
    Signed-off-by: Karl Damkjær Hansen <kdh@es.aau.dk>

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+void banner(void)
+{
+    std::cout << "Genetic Wave" << std::endl;
+    std::cout << "------------" << std::endl;
+}
+
+int main(int argc, char const *argv[])
+{
+    banner();
+    return 0;
+}
